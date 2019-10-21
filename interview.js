0f1687b7f45a9502dba143c71e4c5ecb52f18a14
Shaun Gedye <sgedye88@yahoo.com.au>
2019-10-21 12:20:35 +0800

    Create interview.js

---
+++ interview.js
@@ -0,0 +1,31 @@
+//Appending and Prepending to an array in ES5
+var arrayES5 = ['a', 'b', 'c', 'd'];
+arrayES5.unshift('start');
+arrayES5.push('end');
+console.log(arrayES5); //Output: ['start', 'a', 'b', 'c', 'd', 'end']
+
+//Appending and Prepending to an array in ES6
+var arrayES6 = ['a', 'b', 'c', 'd'];
+arrayES6 = ['start', ...arrayES6];
+arrayES6 = [...arrayES6', 'end'];
+console.log(arrayES6); //Returns ['start', 'a', 'b', 'c', 'd', 'end']
+arrayES6 = ['start', ...arrayES6, 'end']; //To both steps in 1 line of code
+
+
+//How to create a private variable in JavaScript?
+//Create a local variable that only lives within the scope of a function
+function secretVariable() {
+	var private = "super secret code";
+	return function() {
+		return private
+	}
+}
+var getPrivateVariable = secretVariable();
+console.log(getPrivateVariable()); //Output: super secret code
+console.log(secretVariable()); //Output: f() { return private; }
+
+
+//Using the typeof operator
+console.log(typeof typeof 1); //Output: string
+  //typeof 1 = number
+  //typeof (typeof 1) = typeof(number) = string
